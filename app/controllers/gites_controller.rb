20a5c44fbe6a2a2b8a22165620867b6f02035629
Florian de Radiguès <florian.radigues@gmail.com>
2023-09-28 00:00:28 +0200

    cloudinary + starting edit for gites

--- app/controllers/gites_controller.rb
+++ app/controllers/gites_controller.rb
@@ -3,4 +3,14 @@ class GitesController < ApplicationController
     def index
         @gites = Gite.all
     end
+
+    def edit
+        @gite = Gite.find(params[:id])
+    end
+
+    def update
+        raise
+        gite = Gite.find(params[:id])
+    end
+
 end
--- app/models/gite.rb
+++ app/models/gite.rb
@@ -1,4 +1,7 @@
 class Gite < ApplicationRecord
     has_many :charges
     has_many :days_of_weeks
+
+    has_one_attached :main_photo
+    has_many_attached :photos
 end
