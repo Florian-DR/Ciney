2b8d6fd4ef1895ebc094d70c94b3af2307a27704
Florian de Radiguès <florian.radigues@gmail.com>
2023-10-03 23:28:50 +0200

    comments to remember (wasn't safe on previous commit x)

--- app/javascript/controllers/edit_prices_controller.js
+++ app/javascript/controllers/edit_prices_controller.js
@@ -1,30 +1,27 @@
 import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="edit-prices"
+
+// 1. The stimulus data-controller is on the admin.html.erb file
+// 2. Targets on the span with the prices and on the form
+// 3. Action click to toggle d-none classes
+// 4. Action submit on the form
+
 export default class extends Controller {
   static targets = ["priceSemaine","priceWeekEnd", "form", "formSemaine"]
 
-  connect() {
-    console.log("hello")
-  }
-
   displayFormSemaine() {
-    console.log("semaine")
     this.priceSemaineTarget.classList.add("d-none")
     this.formSemaineTarget.classList.remove("d-none")
   }
 
   displayFormWeekEnd() {
-    console.log("week-end")
     this.priceWeekEndTarget.classList.add("d-none")
     this.formTarget.classList.remove("d-none")
   }
 
   update() {
-    // event.preventDefault()
-    const url = this.formTarget.action
-    console.log(this.formTarget)
-    console.log(this.formTarget.action)
+    const url = this.formTarget.action /* /saisons/:id/days_of_week/:id */
     fetch(url, { method: "PATCH", headers: { "Accept": "text/plain" }, body: new FormData(this.formTarget) })
   }
 }
--- app/models/saison.rb
+++ app/models/saison.rb
@@ -3,6 +3,7 @@ class Saison < ApplicationRecord
 
     validates :name, :start_date, :end_date, presence: true
     validates :end_date, comparison: {other_than: :start_date}
+    validates :end_date, comparison: {greater_than: :start_date}
 
     def semaine_price(gite_id)
         days_of_weeks.find{|day| day.status == "semaine" && day.gite_id == gite_id}.price
