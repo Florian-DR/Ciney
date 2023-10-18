a01c83e6b5b6434194af1691fc8b1b383a4c5a01
Florian de Radiguès <florian.radigues@gmail.com>
2023-10-18 21:55:07 +0200

    fixes for deployement

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,16 +1,16 @@
-# class AdminsController < ApplicationController
+class AdminsController < ApplicationController
 
-#     def admin
-#         @saisons = Saison.all
-#         @saison = Saison.new
+    # def admin
+    #     @saisons = Saison.all
+    #     @saison = Saison.new
         
-#         @gite_1 = Gite.first
-#         @gite_2 = Gite.last
+    #     @gite_1 = Gite.first
+    #     @gite_2 = Gite.last
         
-#         @days_of_week = DaysOfWeek.new
-#         @gite_holidays = GiteHoliday.new
+    #     @days_of_week = DaysOfWeek.new
+    #     @gite_holidays = GiteHoliday.new
         
-#         @holidays = Holiday.all
-#         @holiday = Holiday.new
-#     end
-# end
+    #     @holidays = Holiday.all
+    #     @holiday = Holiday.new
+    # end
+end
