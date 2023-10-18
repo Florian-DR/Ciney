68874326903b7a2006c23452e0bf9677b42069de
Florian de Radiguès <florian.radigues@gmail.com>
2023-10-18 21:41:23 +0200

    maileur set up

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -5,6 +5,14 @@ class ContactsController < ApplicationController
 
     def reservation_sender
         email = params[:email]
-        CineyMailer.with(email: email).reservation_mailer.deliver_now
+        gite = params[:gite]
+        capacity = params[:capacity]
+        telephone = params[:telephone]
+        message = params[:message]
+        start_date = params[:start_date]
+        end_date = params[:end_date]
+        CineyMailer.with(email: email, gite: gite, capacity: capacity, telephone: telephone, message: message, start_date: start_date, end_date: end_date).reservation_mailer.deliver_now
+        redirect_to contact_path 
+        flash.notice = "Votre demande à été envoyée, un mail de confirmation devrait suivre"
     end
 end
--- app/mailers/ciney_mailer.rb
+++ app/mailers/ciney_mailer.rb
@@ -1,6 +1,12 @@
 class CineyMailer < ApplicationMailer
     def reservation_mailer
-        email = params[:email]
-        mail(to: email, subject: "Réservation pour les gites d'Auwez")
+        @email = params[:email]
+        @gite = params[:gite]
+        @capacity = params[:capacity]
+        @telephone = params[:telephone]
+        @message = params[:message]
+        @start_date = params[:start_date]
+        @end_date = params[:end_date]
+        mail(bcc: [@email], subject: "Réservation pour les gites d'Auwez")
     end
 end
