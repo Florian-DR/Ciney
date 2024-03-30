a0cffd9d57a8d188096072c06a522730d93c3f45
Florian de Radiguès <florian.radigues@gmail.com>
2024-03-30 18:45:02 +0100

    white space and condition to not crash

--- app/models/home_page.rb
+++ app/models/home_page.rb
@@ -1,5 +1,15 @@
+# class IsSingleton < ActiveModel::Validator
+#     def validate(record)
+#         unless record.class.all.size < 1
+#             record.errors.add :record, "HomePage is a singleton"            
+#         end
+#     end
+# end
+
 class HomePage < ApplicationRecord
     has_many_attached :main_photos
     has_many_attached :entreprises_photos
     has_many_attached :découvrir_photos
+
+    # validates_with IsSingleton
 end
