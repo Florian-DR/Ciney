257578de75fa1af1cf3642c769c0d805f7874f95
Florian de Radiguès <florian.radigues@gmail.com>
2024-08-17 18:15:50 +0200

    fix photos display on safari, remove singleton commented code, add parametrize title

--- app/models/home_page.rb
+++ app/models/home_page.rb
@@ -1,15 +1,5 @@
-# class IsSingleton < ActiveModel::Validator
-#     def validate(record)
-#         unless record.class.all.size < 1
-#             record.errors.add :record, "HomePage is a singleton"            
-#         end
-#     end
-# end
-
 class HomePage < ApplicationRecord
     has_many_attached :main_photos
     has_many_attached :entreprises_photos
     has_many_attached :decouvrir_photos
-
-    # validates_with IsSingleton
 end
