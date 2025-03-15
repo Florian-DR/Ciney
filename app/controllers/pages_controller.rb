b7cfcb6dd5d9774820dc98a9ad8f9aed2b1a3ea8
Florian de Radiguès <florian.radigues@gmail.com>
2025-03-15 17:47:42 +0100

    skip authentification

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: %i[home contact contact_sender]
+  skip_before_action :authenticate_user!, only: %i[home contact contact_sender about]
   def home
     @home = HomePage.first
     # raise
